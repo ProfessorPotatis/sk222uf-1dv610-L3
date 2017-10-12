578d1213ec34a17796a0bdcc2bcfbcc0ac9d83f7
Sofia Kristiansen <sk222uf@student.lnu.se>
2017-10-12 13:25:33 +0200

    Removed files not needed.

--- index.php
+++ index.php
@@ -5,21 +5,19 @@ require_once('model/Session.php');
 require_once('model/Server.php');
 require_once('model/Request.php');
 require_once('model/Get.php');
-require_once('model/Post.php');
 require_once('model/Cookie.php');
-require_once('model/GameDatabase.php');
 require_once('model/DinoGame.php');
 
 require_once('view/LoginView.php');
 require_once('view/RegisterView.php');
-require_once('view/GamesView.php');
+require_once('view/GameView.php');
 require_once('view/DateTimeView.php');
 require_once('view/LayoutView.php');
 
 require_once('controller/RouteController.php');
 require_once('controller/LoginController.php');
 require_once('controller/RegisterController.php');
-require_once('controller/GamesController.php');
+require_once('controller/GameController.php');
 require_once('controller/FormValidator.php');
 
 //MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
