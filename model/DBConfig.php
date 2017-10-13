0b53b8a25abedb1ff8db8e2c5ba57f6a015daaf9
Sofia Kristiansen <sk222uf@student.lnu.se>
2017-10-13 09:52:15 +0200

    Uploading all models and index.php to make sure everything is up to date.

---
+++ model/DBConfig.php
@@ -0,0 +1,13 @@
+<?php
+
+$db_host = 'localhost';
+
+// ON PRODUCTION SITE ONLY!!
+/*$db_user = 'id2850916_ppdb';
+$db_password = 's0S3cRetOMG';
+$db_name = 'id2850916_professorpotatisdb';*/
+
+// ON LOCAL MACHINE ONLY!!
+$db_user = 'root';
+$db_password = 'root';
+$db_name = 'userDB';
